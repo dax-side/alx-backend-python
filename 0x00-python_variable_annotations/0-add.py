24392271b6e700976342c7a74899de5900457407
dax-side <damolaadegbite0@gmail.com>
2023-02-02 23:48:48 +0100

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Defines a type annotated function add """
+
+
+def add(a: float, b: float) -> float:
+    """
+        a type-annotated function that takes
+        param: (float a and float b)
+            and
+        returns: their sum as a float.
+    """
+    return (a + b)
